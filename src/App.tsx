5f119e0ca6ac1268c7651cd66f9a83be9fbf86a7
Ali Mehdi Khan <ali973mehdi@gmail.com>
2026-07-01 20:09:43 +0530

    feat: add route redirect fallback for unknown routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { SearchPage } from "@/pages/SearchPage";
 import { ProfileDetailPage } from "@/pages/ProfileDetailPage";
 import { SelectedListPage } from "@/pages/SelectedListPage";
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<SearchPage />} />
         <Route path="/profile/:username" element={<ProfileDetailPage />} />
         <Route path="/list" element={<SelectedListPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
